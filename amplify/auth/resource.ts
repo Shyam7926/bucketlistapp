504d9fb09a40cd5dc80ba24997b3afff7289cbf8
Shyam7926 <yamala.syamsundar@gmail.com>
2024-11-19 19:04:18 -0600

    'bucketlistapp'

--- amplify/auth/resource.ts
+++ amplify/auth/resource.ts
@@ -1,11 +1,22 @@
-import { defineAuth } from '@aws-amplify/backend';
+import { type ClientSchema, a, defineData } from '@aws-amplify/backend';
 
-/**
- * Define and configure your auth resource
- * @see https://docs.amplify.aws/gen2/build-a-backend/auth
- */
-export const auth = defineAuth({
-  loginWith: {
-    email: true,
+const schema = a.schema({
+  BucketItem: a
+    .model({
+      title: a.string(),
+      description: a.string(),
+      image: a.string(),
+    })
+    .authorization((allow) => [allow.owner()]), // Restrict access to the owner
+});
+
+export type Schema = ClientSchema<typeof schema>;
+
+// Adjust the following to fit the intended purpose of `auth`
+export const auth = defineData({
+  schema,
+  authorizationModes: {
+    defaultAuthorizationMode: 'userPool',
   },
 });
+
